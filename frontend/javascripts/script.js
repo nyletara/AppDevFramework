6da9ec3693cd5062c1cd3fb79bc736d107c0e272
James Xue <james.xue@columbia.edu>
2017-05-09 04:16:54 -0400

    updated front end

--- frontend/javascripts/script.js
+++ frontend/javascripts/script.js
@@ -1,68 +1,115 @@
-var startDate = '';
-var endDate = '';
+var numTweets = '';
 var twitterHandle = '';
 
-$("#startDate").change(function(){
-    startDate = $('#startDate').val();
-    if ((startDate != '') && (endDate != '') && (twitterHandle != '')){
-        console.log(twitterHandle, startDate, endDate)
-        create_graph(twitterHandle, startDate, endDate)
-    }
-})
-
-$("#endDate").change(function(){
-    endDate = $('#endDate').val();
-    if ((startDate != '') && (endDate != '') && (twitterHandle != '')){
-        console.log(twitterHandle, startDate, endDate)
-        create_graph(twitterHandle, startDate, endDate)
+$("#numTweets").change(function(){
+    numTweets = $('#numTweets').val();
+    if ((numTweets != '') && (twitterHandle != '')){
+        console.log(twitterHandle, numTweets)
+        get_data(twitterHandle, numTweets)
     }
 })
 
 $("#twitterHandle").change(function(){
     twitterHandle = $('#twitterHandle').val();
-    if ((startDate != '') && (endDate != '') && (twitterHandle != '')){
-        console.log(twitterHandle, startDate, endDate)
-        create_graph(twitterHandle, startDate, endDate)
+    if ((numTweets != '') && (twitterHandle != '')){
+        console.log(twitterHandle, numTweets)
+        get_data(twitterHandle, numTweets)
     }
 })
 
-create_graph('01/01/2016', '04/16/2017')
 
-function create_graph(twitterHandle, startDate, endDate) {
+// some examples: ishantlguru, realDonaldTrump, justinbieber
+get_data('realDonaldTrump', '10');
+
+function get_data(twitterHandle, numTweets) {
+    var startTime = new Date();
     $('#container').append('<div style="height: 800px" id="total_chart"></div>');
 
-    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
-        console.log(data);
+    $.getJSON('https://ffsv418b51.execute-api.us-west-2.amazonaws.com/production/getsentimentdata/' + twitterHandle + '/' + numTweets, function (data) {
+
+        console.log(data)
 
         series_list = []
+
+        new_data_angry = []
+        for (index in data['angry']){
+            new_data_angry.push(data['angry'][index].slice(1, 3))
+        }
+
+        new_data_joy = []
+        for (index in data['joy']){
+            new_data_joy.push(data['joy'][index].slice(1, 3))
+        }
+
+        new_data_sadness = []
+        for (index in data['sadness']){
+            new_data_sadness.push(data['sadness'][index].slice(1, 3))
+        }
+
+        new_data_fear = []
+        for (index in data['fear']){
+            new_data_fear.push(data['fear'][index].slice(1, 3))
+        }
+
+        new_data_disgust = []
+        for (index in data['disgust']){
+            new_data_disgust.push(data['disgust'][index].slice(1, 3))
+        }
+
         series_list.push({
             type: 'line',
-            name: 'Anger',
-            color: '#BF0B23',
-            data: data.slice(0, 300)
+            name: 'Angry',
+            color: '#EC4422',
+            data: new_data_angry
         })
 
         series_list.push({
             type: 'line',
-            name: 'Happiness',
-            color: '#008000',
-            data: data.slice(301, 500)
+            name: 'Joy',
+            color: '#F8E250',
+            data: new_data_joy
         })
 
         series_list.push({
             type: 'line',
             name: 'Sadness',
-            color: '#0000ff',
-            data: data.slice(501, 600)
+            color: '#2A58A5',
+            data: new_data_sadness
         })
 
+        series_list.push({
+            type: 'line',
+            name: 'Fear',
+            color: '#8258A2',
+            data: new_data_fear
+        })
 
+        series_list.push({
+            type: 'line',
+            name: 'Disgust',
+            color: '#73BB4B',
+            data: new_data_disgust
+        })
+
+        console.log(series_list);
+
+        create_chart(twitterHandle);
+        var endTime = new Date();
+        var timeDiff = endTime - startTime;
+        timeDiff /= 1000;
+        console.log(timeDiff);
+    });
+}
+
+
+ function create_chart(twitterHandle) {
+        
         Highcharts.chart('container', {
             chart: {
                 zoomType: 'x'
             },
             title: {
-                text: 'Tweet Sentiment Data for user over time'
+                text: 'Tweet Sentiment Data for @' + twitterHandle + ' over time'
             },
             subtitle: {
                 text: document.ontouchstart === undefined ?
@@ -108,5 +155,4 @@ function create_graph(twitterHandle, startDate, endDate) {
 
             series: series_list
         });
-    });
 }
